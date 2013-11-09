2cff7b659cf569d6074c2a3ca1f069df7673785e
jdmooney <JacobDanielMooney@gmail.com>
2013-11-09 17:50:59 -0600

    Challenge code for session 1 has been committed.

---
+++ Session1_Challenges/6_string.rb
@@ -0,0 +1,25 @@
+# You'll get a string and a boolean.
+# When the boolean is true, return a new string containing all the odd characters.
+# When the boolean is false, return a new string containing all the even characters.
+# 
+# If you have no idea where to begin, remember to check out the cheatsheets for string and logic/control
+# 
+# odds_and_evens("abcdefg",true)    # => "bdf"
+# odds_and_evens("abcdefg",false)   # => "aceg"
+
+def odds_and_evens(string, return_odds)
+	returnString = ""
+	string.chars.count.times do |count|
+		if return_odds
+			if count.odd?
+				returnString << string[count]
+			end
+		else
+			if count.even?
+				returnString << string[count]
+			end
+		end
+	end
+	returnString
+end
+
---
+++ Session1_Challenges/7_string.rb
@@ -0,0 +1,33 @@
+# given a string, return the character after every letter "r"
+
+# 
+
+# pirates_say_arrrrrrrrr("are you really learning Ruby?") # => "eenu"
+
+# pirates_say_arrrrrrrrr("Katy Perry is on the radio!")   # => "rya"
+
+# pirates_say_arrrrrrrrr("Pirates say arrrrrrrrr")        # => "arrrrrrrr"
+
+
+
+def pirates_say_arrrrrrrrr(string)
+
+	counter = 0
+	limiter = string.chars.count
+	returnString = ""
+
+	while counter < limiter
+		if string[counter].downcase == "r"
+			if string[counter+1]
+				returnString << string[counter+1]
+			end
+		end
+		counter += 1
+	end
+	returnString
+end
+
+
+
+pirates_say_arrrrrrrrr("Pirates say arrrrrrrrr")
+
